40340f15c36eff8ec836bc9aec8f044893233dfe
jaabanks <86501922+jaabanks@users.noreply.github.com>
2021-09-22 08:34:03 -0500

    Update internet_gateway.tf

--- internet_gateway.tf
+++ internet_gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_internet_gateway" "Project_IG" {
-  vpc_id = aws_vpc.Project_Terraform_VPC  .id
+  vpc_id = aws_vpc.Project_Terraform_VPC.id
   
   tags = {
     Name = "Project Internet Gateway"
